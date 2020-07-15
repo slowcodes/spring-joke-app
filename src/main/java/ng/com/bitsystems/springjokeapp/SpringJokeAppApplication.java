e5cb9dbe4438f5527ace5faf08f03dbe65f1f9fc
Ezenna Kelechi <charlesezenna@gmail.com>
2020-07-15 23:19:43 +0100

    Substituted xml configuration with java configuration while still injecting configured beans

--- src/main/java/ng/com/bitsystems/springjokeapp/SpringJokeAppApplication.java
+++ src/main/java/ng/com/bitsystems/springjokeapp/SpringJokeAppApplication.java
@@ -1,9 +1,12 @@
 package ng.com.bitsystems.springjokeapp;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:chuck-config.xml")
 public class SpringJokeAppApplication {
 
     public static void main(String[] args) {
